40a0fd630d6dce03574292617ce6fe5fddea26d2
2323kw-ops <23239kw@hvhs.school.nz>
2026-02-04 15:09:50 +1300

    4/2

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function start() {
     alert("alert\nYou got the script running.")
 }
+
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+  alert("Your name is "+userName+" .")
+}
+
+function useInput() {
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("welcomeMessage").innerHTML =
+    "Hello " + name;
+}
